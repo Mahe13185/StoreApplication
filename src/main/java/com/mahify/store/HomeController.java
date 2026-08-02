daaa086ee40ba31596bfc9f78e23ad8d9609559a
Mahendra Gampala <mahendragampala13185@gmail.com>
2026-08-02 14:56:53 +0530

    Learned About DI and IOC

---
+++ src/main/java/com/mahify/store/HomeController.java
@@ -0,0 +1,17 @@
+package com.mahify.store;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @Value("${spring.application.name}")
+    private String name;
+
+    @RequestMapping("/")
+    public String index(){
+        System.out.println(name);
+        return "index.html";
+    }
+}
