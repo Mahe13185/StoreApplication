6d83bd4266aac7c27f8ef2141d91729bd964bee4
Mahendra Gampala <mahendragampala13185@gmail.com>
2026-08-05 13:17:05 +0530

    9. Externalizing Configuration last file

--- src/main/java/com/mahify/store/StripePaymentService.java
+++ src/main/java/com/mahify/store/StripePaymentService.java
@@ -1,15 +1,35 @@
 package com.mahify.store;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service("stripe")
 @Primary
 public class StripePaymentService implements PaymentService {
+    @Value("${stripe.apiUrl}")
+    private String apiUrl;
+
+    @Value("${stripe.enabled}")
+    private Boolean enabled;
+
+    @Value("${stripe.timeout}")
+    private int timeout;
+
+    @Value("${stripe.supported-currencies}")
+    private List<String> supported_currencies;
+
     @Override
     public void processPayment(double amount){
         System.out.println("Stripe");
+        System.out.println("Api URL: " + apiUrl);
+        System.out.println("Enabled: " + enabled);
+        System.out.println("TimeOut: " + timeout);
+        System.out.println("Supported Currencies: " + supported_currencies);
         System.out.println("Amount" + amount);
+
     }
 }
