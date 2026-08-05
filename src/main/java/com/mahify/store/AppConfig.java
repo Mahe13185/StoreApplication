9cc2ceb9dd7fdacefe11a97dbf2e6e6b8d0c8411
Mahendra Gampala <mahendragampala13185@gmail.com>
2026-08-05 13:27:16 +0530

    10.1 implemented the if condition so that if we change the yaml file the payment type will changed

--- src/main/java/com/mahify/store/AppConfig.java
+++ src/main/java/com/mahify/store/AppConfig.java
@@ -1,19 +1,31 @@
 package com.mahify.store;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.sql.Struct;
 
 @Configuration
 public class AppConfig {
 
+    @Value("${payment_Gateway}")
+    private String paymentGateway;
     @Bean
     public PaymentService stripe(){
         return new StripePaymentService();
     }
+    @Bean
+    public PaymentService paypal(){
+        return new PaypalPaymentService();
+    }
 
     @Bean
     public OrderService orderService(){
-        return new OrderService(stripe());
+        if (paymentGateway.equals("stripe")){
+            return new OrderService(stripe());
+        }
+        return new OrderService(paypal());
     }
 }
