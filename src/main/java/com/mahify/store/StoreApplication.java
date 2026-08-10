bc347d33c9fa430d092412cbd6ebe2446d7256ea
Mahendra Gampala <mahendragampala13185@gmail.com>
2026-08-10 20:44:02 +0530

    6.1 and 6.2 added the database migration with flyway

--- src/main/java/com/mahify/store/StoreApplication.java
+++ src/main/java/com/mahify/store/StoreApplication.java
@@ -9,11 +9,5 @@ public class StoreApplication {
 
     public static void main(String[] args) {
         ApplicationContext context =  SpringApplication.run(StoreApplication.class, args);
-       var userService =  context.getBean(UserService.class);
-       userService.registerUser(new User(1l,"mahendraGampala2005@gmail.com","mahendra","Mahendra"));
-       userService.registerUser(new User(1l,"mahendraGampala2005@gmail.com","mahendra","Mahendra"));
-
-       //        var orderService = context.getBean(OrderService.class);
-//        orderService.placeOrder();
     }
 }
